af350ec342f8d704b59f86c3bfd3f07dbdc0eaa2
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2016-10-30 19:58:23 +0100

    Filtered String

--- src/language/LanguageClassification.java
+++ src/language/LanguageClassification.java
@@ -2,10 +2,12 @@ package language;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +25,7 @@ public class LanguageClassification {
 		for (Path path : directoryStream) {
 			Path file = path.getFileName();
 			String languageName = file.toString().split("\\.")[0];
-			String content = readFileContent(path);
+			String content = readFileContentFiltered(path);
 			double compressionRate = calculateCompressionRate(content);
 			classes.add(new Language(languageName,compressionRate,content));
 			System.out.println("name: " + languageName + ", compression rate: " + compressionRate); 
@@ -33,13 +35,13 @@ public class LanguageClassification {
 		directoryStream = Files.newDirectoryStream(testFilePath);
 		for (Path path : directoryStream) {
 			Path file = path.getFileName();
-			String content = readFileContent(path);
+			String content = readFileContentFiltered(path);
 			Language language = null;
 			double diffToCurrent = 0;
 			for(Language curLanguage : classes){
 				double compressionRate = calculateCompressionRate(curLanguage.getReferenceText() + content);
 				System.out.print("File " + file.toString() + " as " + curLanguage.getName() + ": " + compressionRate);
-				double diff = compressionRate - curLanguage.getCompressionRate();
+				double diff = Math.abs(curLanguage.getCompressionRate() - compressionRate);
 				System.out.println(" - Delta " + diff);
 				if(diffToCurrent > diff || language == null){
 					diffToCurrent = diff;
@@ -51,25 +53,35 @@ public class LanguageClassification {
 			
 	}
 	
-	public static byte[] compress(String str) {
+	public static byte[] compress(String str) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try {
-            OutputStream out = new DeflaterOutputStream(baos);
-            out.write(str.getBytes("UTF-8"));
-            out.close();
-        } catch (IOException e) {
-            throw new AssertionError(e);
-        }
+        OutputStream out = new DeflaterOutputStream(baos);
+        out.write(str.getBytes("UTF-8"));
+        out.close();
         return baos.toByteArray();
     }
 	
-	public static double calculateCompressionRate(String str){
+	public static double calculateCompressionRate(String str) throws IOException{
 		byte[] compressed = compress(str);
-		double compressionRate = (double) compressed.length/str.getBytes().length;
+		double compressionRate = (double) compressed.length/str.getBytes("UTF-8").length;
 		return compressionRate;
     }
 	
-	public static String readFileContent(Path file) throws IOException{
+	private static String readFileContent(Path file) throws IOException{
 		return Files.readAllLines(file).stream().collect(Collectors.joining("\n"));
 	}
+	
+	private static String readFileContentFiltered(Path file) throws IOException{
+		return filterString(readFileContent(file));
+	}
+
+	private static String filterString(String unfiltered) {
+		String filtered = unfiltered;
+		filtered = Normalizer.normalize(filtered, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+		filtered = filtered.replaceAll("\\s+","");
+		filtered = filtered.toLowerCase();
+		return filtered;
+	}
+	
+	
 }
